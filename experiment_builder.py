323cfb4fcd329be40eb921692d5def78e4bc2a78
msmilevski1 <marko.smilevski@eriden.io>
2019-10-31 19:13:16 +0100

    Add skeleton of experiment builder class

---
+++ experiment_builder.py
@@ -0,0 +1,27 @@
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+class ExperimentBuilder(object):
+    def __init__():
+        pass
+    
+    def train():
+        pass
+
+    def run_train_iter():
+        pass
+
+    def run_eval_iter():
+        pass
+
+    def save_model():
+        pass
+
+    def load_model():
+        pass
+
+
+
+
